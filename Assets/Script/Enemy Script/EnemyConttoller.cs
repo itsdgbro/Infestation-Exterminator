1c3730c9e9ddd59abcdb0bafe872e856cf7de9a3
Dipak Gurung <dipakgrg524@gmail.com>
2024-02-17 22:17:24 +0545

    Enemy FOV

---
+++ Assets/Script/Enemy Script/EnemyConttoller.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyConttoller : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    Rigidbody rb;
+    Camera viewCamera;
+    Vector3 velocity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        viewCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        Vector3 mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x , Input.mousePosition.y, viewCamera.transform.position.y));
+        transform.LookAt(mousePos + Vector3.up * transform.position.y);
+        velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+    }
+}
